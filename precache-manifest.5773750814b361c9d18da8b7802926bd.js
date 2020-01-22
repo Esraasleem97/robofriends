6c82a0271b1b879353adedcb364299bf306f0ede
Esraasaleem97 <es1915@live.com>
2020-01-22 10:58:50 +0200

    Updates

---
+++ precache-manifest.5773750814b361c9d18da8b7802926bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a94c54a486874a554055a6aefab804",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "21ff5ee0cc144b306502",
+    "url": "/robofriends/static/css/main.b3bc9d44.chunk.css"
+  },
+  {
+    "revision": "16a1c88530af2965fc64",
+    "url": "/robofriends/static/js/2.c2d93e68.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.c2d93e68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21ff5ee0cc144b306502",
+    "url": "/robofriends/static/js/main.5313ae90.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
